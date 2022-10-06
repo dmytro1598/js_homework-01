20b12a17f76d66fcb131bf647f009d51bd8399bb
dmytro1598 <79602161+dmytro1598@users.noreply.github.com>
2022-10-06 04:37:00 +0300

    Добавил текст

---
+++ index.js
@@ -0,0 +1,21 @@
+// // Ex#1
+
+// // Change code below this line
+// const productName = "Droid";
+// // console.log(productName);
+// // 'Droid'
+// const pricePerItem  = 2000;
+// // console.log(pricePerItem);
+// // 2000
+
+// 
+function filterArray(numbers, value) {
+  numbers = 0;
+  for (let i = numbers; i > value; i += 1) {
+    numbers.push(i);
+  
+    return numbers;
+  }
+}
+
+// console.filterArray([1, 2, 3, 4, 5], 3);
